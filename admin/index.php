32de81c3badaf29a4850ece9bb3f769991750f05
Pierrick LEFEBVRE <plefebvr@e1r7p12.42.fr>
2018-06-02 16:28:55 +0200

    miaou

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+
+<?php
+
+//Check si l'user est bien un admoin avant de lui laisser l'acces au panel.
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+            <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Admin Panel </title>
+    </head>
+    <body>
+      <h1 style="width:100%;text-align: center;">Admin Panel</h1>
+      <div class="square-admin"><h2>Categories</h2></div>
+      <div class="square-admin"><h2>Products</h2></div>
+      <div class="square-admin"><h2>Users</h2></div>
+      <div class="square-admin"><h2>Settings</h2></div>
+    </body>
+</html>
