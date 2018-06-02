b242c3c8cf6619de7844f1db5cc07b035f95cfe9
Lola OLIVET <lolivet@student.42.fr>
2018-06-02 16:43:05 +0200

    connexion

--- connexion.php
+++ connexion.php
@@ -4,9 +4,9 @@ if ($bdd = mysqli_connect('localhost', 'root', 'rootroot', 'ecommerce'))
 {
     echo "Bienvenue";
     $resultat = mysqli_query($bdd, 'SELECT * FROM `products`');
-    while ($donnees = mysqli_fetch_assioc($resultat))
+    while ($donnees = mysqli_fetch_assoc($resultat))
     {
-        echo $donnees['id_product']."\n".$donnees['name'];
+        echo "</br>".$donnees['id_product']."</br>".$donnees['name']."</br>";
     }
     mysqli_free_result($resultat);
 }
